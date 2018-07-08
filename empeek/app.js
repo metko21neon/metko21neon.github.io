94b96e60e38224cd1c2b32d29ed37d82e7696c29
Igor Metko <metko21neon@gmail.com>
2018-07-09 02:34:30 +0300

    Empeek delete
    Empeek delete

--- empeek/app.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('myApp', [
-    'myApp.comments',
-    'ngRoute'
-]).
-config(['$locationProvider', function($locationProvider) {
-    $locationProvider.html5Mode(true);
-}]);
