925f50b4648ad05cf5de7d72ad5720c814ad0854
Igor Metko <metko21neon@gmail.com>
2018-08-08 20:24:51 +0300

    Bitokkk
    1

---
+++ bitokkk/js/common.js
@@ -0,0 +1,80 @@
+$(function() {
+
+    //Mixitup
+    var mixer = mixitup('.team__list');
+
+    //Timeline
+    $('.timeline').timeline({
+        mode: 'horizontal',
+        forceVerticalMode: 600,
+        visibleItems: 3
+    });
+
+    //PageScrollToId
+    $("a[rel='m_PageScroll2id']").mPageScroll2id({
+        offset: 72
+    });
+
+    /*//Flip-clock
+    var clock = new FlipClock($('.header__timer'), {
+        clockFace: "DailyCounter",
+        autoStart: true,
+        countdown: true
+    });
+    clock.setTime(50000);
+    clock.setCountdown(true);
+    clock.start();*/
+
+    //Owl-carousel
+    var owl = $('.owl-carousel');
+    owl.owlCarousel({
+        loop:true,
+        autoplay:true,
+        autoplayTimeout:3000,
+        autoplayHoverPause:true,
+        responsive:{
+            0:{
+                items:2
+            },
+            991:{
+                items:3
+            },
+            1199:{
+                items:5
+            }
+        }
+    });
+    $('.play').on('click',function(){
+        owl.trigger('play.owl.autoplay',[1000])
+    });
+    $('.stop').on('click',function(){
+        owl.trigger('stop.owl.autoplay')
+    });
+
+    //Animation
+    new WOW().init();
+
+    //DownButton
+    $(function() {
+        $('a[href*=#]').on('click', function(e) {
+            e.preventDefault();
+            $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 1500, 'easeInExpo');
+        });
+    });
+
+    //Button to top
+    $("body").on("click", ".top", function(){
+        $("html, body").animate({scrollTop: 0},"slow");
+    });
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 400) {
+            $(".top").addClass("top__active");
+        } else {
+            $(".top").removeClass("top__active");
+        }
+    });
+
+    $("body").append('<div class="top wow pulse infinite d-none d-md-block" data-wow-duration="2.5s"><i class="fa top__fa fa-angle-up"></i></div>');
+
+});
