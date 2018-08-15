8a1ceaa58e3e4d19df3b4779c59afca15618cecf
Igor Metko <metko21neon@gmail.com>
2018-08-15 15:52:14 +0300

    Bitok

--- bitok/js/common.js
+++ bitok/js/common.js
@@ -11,29 +11,19 @@ $(function() {
     });
 
     //Mixitup
-    var mixer = mixitup('.team__list');
-
-    //Timeline
-    /*$('.timeline').timeline({
-        mode: 'horizontal',
-        forceVerticalMode: 600,
-        visibleItems: 3
-    });*/
+    mixitup('.team__list', {
+        selectors: {
+            control: '[data-mixitup-control]'
+        },
+        animation: {
+            duration: 300
+        }
+    });
 
     //PageScrollToId
     $("a[rel='m_PageScroll2id']").mPageScroll2id({
         offset: 66
     });
-
-    /*//Flip-clock
-    var clock = new FlipClock($('.header__timer'), {
-        clockFace: "DailyCounter",
-        autoStart: true,
-        countdown: true
-    });
-    clock.setTime(50000);
-    clock.setCountdown(true);
-    clock.start();*/
 
     //Owl-carousel
     var owl = $('.owl-carousel');
@@ -66,28 +56,5 @@ $(function() {
 
     //Animation
     new WOW().init();
-
-    //DownButton
-    $(function() {
-        $('a[href*=#]').on('click', function(e) {
-            e.preventDefault();
-            $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 1500, 'easeInExpo');
-        });
-    });
-
-    //Button to top
-    $("body").on("click", ".top", function(){
-        $("html, body").animate({scrollTop: 0},"slow");
-    });
-
-    $(window).scroll(function() {
-        if ($(this).scrollTop() > 400) {
-            $(".top").addClass("top__active");
-        } else {
-            $(".top").removeClass("top__active");
-        }
-    });
-
-    $("body").append('<div class="top wow pulse infinite d-none d-md-block" data-wow-duration="2.5s"><i class="fa top__fa fa-angle-up"></i></div>');
 
 });
